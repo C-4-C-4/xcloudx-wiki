0dceec97a657ee15cb5061fafc456ce35b5a676c
C-4-C-4 <cccc4444@aliyun.com>
2025-05-03 20:19:33 +0800

    添加了信ICP备案号

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -139,8 +139,8 @@ export default defineConfig({
       { icon: 'github', link: 'https://github.com/C-4-C-4/xcloudx-wiki' }
     ],
     footer: {
-      message: '<a href="https://www.govs.cc/id.php?keyword=20252025" target="_blank">嘣ICP备20252025号</a>',
-      copyright: 'Copyright © 2024-present XCloudX'
+      message: '<a href="https://icp.govs.xin/id.php?keyword=20252025" target="_blank">信ICP备20252025号</a>',
+      copyright: '<a href="https://www.govs.cc/id.php?keyword=20252025" target="_blank">嘣ICP备20252025号</a>',
     }
   }
 })
